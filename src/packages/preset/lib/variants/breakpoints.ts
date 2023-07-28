c00c9010636c98c41b0793f454398ea6f2af6a44
Pradipsinh Jadeja <mrjadeja.in@gmail.com>
2023-07-28 12:44:31 +0530

    Add only breakpoints | Prevent zero from override

--- src/packages/preset/lib/variants/breakpoints.ts
+++ src/packages/preset/lib/variants/breakpoints.ts
@@ -20,10 +20,16 @@ import type { Breakpoints, PluginProps } from "..";
  * `x-y:` - where **x** and **y** is breakpoint keys, x is not *zero*
  */
 export default function ({ theme, addVariant }: PluginProps): void {
+  // unsorted breakpoints
+  const unsortedBreakpoints = (theme("breakpoints") || {}) as Breakpoints;
+  // make sure zero has 0
+  unsortedBreakpoints["zero"] = 0;
+
   // ascending order of breakpoint values
-  const breakpoints = Object.entries(
-    (theme("breakpoints") || {}) as Breakpoints
-  )?.sort((a, b) => a?.[1] - b?.[1]);
+  const breakpoints = Object.entries(unsortedBreakpoints)?.sort(
+    (a, b) => a?.[1] - b?.[1]
+  );
+
   breakpoints?.forEach(([key, value], index) => {
     if (key === "zero") {
       // generate zero-*: breakpoint variants
@@ -43,18 +49,35 @@ export default function ({ theme, addVariant }: PluginProps): void {
     } else {
       // generates minimum width breakpoint variants
       addVariant(key, `@media (min-width: ${value}px)`);
-      // generate x-y: breakpoint variants where x is not zero
-      // For example,
-      // let's take one breakpoint, lg: 1024
-      // let's take another breakpoitn, md: 768
-      // then md-lg: variant applies on the 768-1023 px viewport
-      // lg-md doesn't make any sense
+      // range and only breakpoints
       breakpoints.forEach(([_key, _value], _index) => {
-        _index > index &&
+        /* range breakpoints */
+        // generate x-y: breakpoint variants where x is not zero
+        // For example,
+        // let's take one breakpoint, lg: 1024
+        // let's take another breakpoitn, md: 768
+        // then md-lg: variant applies on the 768-1023 px viewport
+        // lg-md doesn't make any sense
+        if (_index > index) {
           addVariant(
             `${key}-${_key}`,
             `@media (min-width: ${value}px) and (max-width: ${_value - 1}px)`
           );
+        }
+        /* only breakpoints */
+        // For example,
+        // zero-only: not generated as first breakpoint by value
+        // xs-only: 375-639 px viewport
+        // sm-only: 640-767 px viewport
+        // ...
+        // mac-only: 1440-1919 px viewport
+        // maxw-only: not generated as last breakpoint by value
+        if (index + 1 === _index) {
+          addVariant(
+            `${key}-only`,
+            `@media (min-width: ${value}px) and (max-width: ${_value - 1}px)`
+          );
+        }
       });
     }
   });
